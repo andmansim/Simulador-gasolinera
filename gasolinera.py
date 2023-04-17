2a94208fe795b20c7c6964dd97db116303a553e4
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-17 17:00:40 +0200

    enunciado

---
+++ gasolinera.py
@@ -0,0 +1,10 @@
+#indicar los estados: 
+#coches hilos, 50 hilos
+#tiempo 15 minutos, surtidores 1
+#calcular el tiempo medio que tarda un coche
+#pasar todos los cálculos a centensimas de segundo
+
+#llegan coches cada 15 minutos, gasolinera 1 surtidor. 
+#Coche en el surtidor tarda entre 5 y 10 minutos
+#Después se va a pagar: se pone en la caja, solo hay 1, tarda 3 minutos
+#Tras terminar se va
