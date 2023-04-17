9edf3baca613662ab8c996ab6e2b5c9d85fb44a0
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-17 17:04:28 +0200

    empezamos colas

--- gasolinera.py
+++ gasolinera.py
@@ -8,3 +8,17 @@
 #Coche en el surtidor tarda entre 5 y 10 minutos
 #Después se va a pagar: se pone en la caja, solo hay 1, tarda 3 minutos
 #Tras terminar se va
+
+import threading
+
+'''
+Necesitamos: cola, cliente, gasolinera
+'''
+class Cola:
+    def __ini__(self):
+        self.datos = []
+    
+    def encolar(self, x):
+        self.datos.append(x)
+    def 
+        
